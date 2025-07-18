726ebdcf79b30e2123b3f518b2c19fe1cb547bd6
Akshit <akshitg43@gmail.com>
2025-07-18 18:34:06 -0400

    Fix: remove access_key placeholder

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -16,11 +16,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name = var.vm_name
   location = var.location
   resource_group_name = var.resource_group_name
-  vm_size = var.vm_size
+  size = var.vm_size
   admin_username = var.admin_username
   admin_password = var.admin_password
-  network_interface_ids = 
-    var.network_interface_ids
+  network_interface_ids = var.network_interface_ids
 
  os_disk {
     name                 = var.os_disk.name
