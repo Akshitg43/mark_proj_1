9fead49dd77b93691bcadcb302becfeb3a444f28
Akshit <akshitg43@gmail.com>
2025-07-27 14:35:28 -0400

    l

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name = "jenkins"
     storage_account_name = "terraform999"
-    container_name = "terraform"
+    container_name = "terraform999"
     key = "terraform.tfstate"
   }
 }
